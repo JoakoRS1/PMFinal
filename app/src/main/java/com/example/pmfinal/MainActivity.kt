e7313a412dde8f7d51c2a91af121bc8faa343599
Vane-byte <20180841@aloe.ulima.edu.pe>
2022-07-16 18:09:33 -0500

    Todo Intro (menos edad, que me da flojera) yya está el nav bar

--- app/src/main/java/com/example/pmfinal/MainActivity.kt
+++ app/src/main/java/com/example/pmfinal/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.pmfinal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.pmfinal.NavFragments.CalculatorFragment
+import com.example.pmfinal.NavFragments.FitnessFragment
+import com.example.pmfinal.NavFragments.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity: AppCompatActivity() {
+    private val calculator= CalculatorFragment()
+    private val fitness= FitnessFragment()
+    private val settings= SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(fitness)
+
+        val bottomNav= findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.calc -> replaceFragment(calculator)
+                R.id.settings->replaceFragment(settings)
+                R.id.fit -> replaceFragment(fitness)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment:Fragment){
+        if (fragment!=null){
+            val ft= supportFragmentManager.beginTransaction()
+            ft.replace(R.id.fragment_container_main,fragment)
+            ft.addToBackStack(null)
+            ft.commit()
+        }
     }
 }
