05dcb783aaafa9926393e31ad3f90a5d8a8d65eb
Joaquin <20181659@aloe.ulima.edu.pe>
2022-07-17 12:07:31 -0500

    Calculadora

---
+++ app/src/main/java/com/example/pmfinal/settings/perfilFragment.kt
@@ -0,0 +1,18 @@
+package com.example.pmfinal.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pmfinal.R
+
+class perfilFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settingsperfil, container, false)
+    }
+}
