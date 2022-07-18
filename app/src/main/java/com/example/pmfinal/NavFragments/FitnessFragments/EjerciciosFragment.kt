b23b7439b3d43c3a2acae1aebf1a6e98dd3b1b0c
sam <sam.perezf@hotmail.com>
2022-07-18 02:21:15 -0500

    nuevo ejercicio 3.0

--- app/src/main/java/com/example/pmfinal/NavFragments/FitnessFragments/EjerciciosFragment.kt
+++ app/src/main/java/com/example/pmfinal/NavFragments/FitnessFragments/EjerciciosFragment.kt
@@ -1,17 +1,23 @@
 package com.example.pmfinal.NavFragments.FitnessFragments
 
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pmfinal.R
 import com.example.pmfinal.adapters.ListadoExercisesAdapter
 import com.example.pmfinal.models.GestorEjercicios
+import com.example.pmfinal.settings.perfilFragment
 
 class EjerciciosFragment:Fragment() {
+
+    private val nuevo= NuevoEjerFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -54,6 +60,16 @@ class EjerciciosFragment:Fragment() {
             ft.addToBackStack(null)
             ft.commit()
         }
+
+        val layoutNuevo = view.findViewById<LinearLayout>(R.id.layoutNuevo)
+
+        layoutNuevo.setOnClickListener {
+            val ft = requireActivity().supportFragmentManager?.beginTransaction()
+            ft!!.replace(R.id.fragment_container_main, nuevo)
+            ft!!.addToBackStack(null)
+            ft!!.commit()
+        }
+
 
         recyclerView.adapter=adapter
     }
---
+++ app/src/main/java/com/example/pmfinal/NavFragments/FitnessFragments/NuevoEjerFragment.kt
@@ -0,0 +1,32 @@
+package com.example.pmfinal.NavFragments.FitnessFragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.Toast
+import androidx.core.app.NotificationManagerCompat
+import androidx.fragment.app.Fragment
+import com.example.pmfinal.R
+
+class NuevoEjerFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_nuevo_ejer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
+    }
+
+
+}
