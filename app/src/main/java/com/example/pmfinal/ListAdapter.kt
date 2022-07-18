c7c84a210f7337b7a549f63b901187ff14a4647e
Joaquin <20181659@aloe.ulima.edu.pe>
2022-07-18 00:11:17 -0500

    Calculadora

--- app/src/main/java/com/example/pmfinal/ListAdapter.kt
+++ app/src/main/java/com/example/pmfinal/ListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.pmfinal
 
 import android.content.Context
+import android.content.DialogInterface
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
@@ -8,47 +9,42 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.TextView
 
- class ListAdapter(lista :  MutableList<RM>, context:Context) :RecyclerView.Adapter<ListAdapter.ViewHolder>(){
-     private var lista = mutableListOf<RM>()
-     private lateinit var mInflater : LayoutInflater
-     private lateinit var context : Context
+ class ListAdapter(private val mlistaRM : List<RM>, private val mOnClickListener: (rm : RM)->Unit)
+     : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+         val tvRm: TextView
+         val tvfecha: TextView
+         val tvMax: TextView
 
-     fun ListAdapter(lista :  MutableList<RM>, context:Context){
-         this.mInflater = LayoutInflater.from(context )
-         this.context = context
-         this.lista = lista
+         init {
+             tvRm = view.findViewById(R.id.peso)
+             tvfecha = view.findViewById(R.id.fecha)
+             tvMax = view.findViewById(R.id.Maximo)
+         }
+
+
      }
+
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-         var view =mInflater.inflate(R.layout.list_listas_1rm,null)
-         return ViewHolder(view)
+         val view = LayoutInflater.from(parent.context)
+             .inflate(R.layout.list_listas_1rm, parent, false)
+
+         val viewHolder =  ViewHolder(view)
+         return viewHolder
      }
 
      override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-         holder.bindData(lista.get(position))
+         val rm = mlistaRM[position]
+         holder.tvRm.text = rm.repM
+         holder.tvfecha.text = rm.fecha
+         holder.tvMax.text = rm.max
+         holder.itemView.setOnClickListener {
+             mOnClickListener(rm)
+         }
+
      }
 
      override fun getItemCount(): Int {
-         return lista.size
+         return mlistaRM.size
      }
-     class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
-
-
-         val tvrm : TextView
-         val tvfecha : TextView
-         val tvmax : TextView
-         init{
-             tvrm  =  view.findViewById(R.id.peso)
-             tvfecha = view.findViewById(R.id.fecha)
-             tvmax = view.findViewById(R.id.Maximo)
-         }
-
-         fun bindData(rm:RM) {
-             tvrm.setText(rm.repM)
-             tvfecha.setText(rm.fecha)
-             tvmax.setText(rm.max)
-         }
-     }
-
-
-
  }
