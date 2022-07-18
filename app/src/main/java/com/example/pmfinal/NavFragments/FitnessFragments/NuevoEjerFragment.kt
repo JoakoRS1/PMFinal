2a1d56754a51432dd6a9188e0e14a8a54f76ac62
sam <sam.perezf@hotmail.com>
2022-07-18 02:38:03 -0500

    nuevo ejercicio 4.0

--- app/src/main/java/com/example/pmfinal/NavFragments/FitnessFragments/NuevoEjerFragment.kt
+++ app/src/main/java/com/example/pmfinal/NavFragments/FitnessFragments/NuevoEjerFragment.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Switch
-import android.widget.Toast
+import android.widget.*
 import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import com.example.pmfinal.R
@@ -22,11 +21,30 @@ class NuevoEjerFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val eteNombre= view.findViewById<TextView>(R.id.eteNombre)
+        val butElegir= view.findViewById<Button>(R.id.butElegir)
 
+        butElegir.setOnClickListener{
+            if (eteNombre.text.toString() != ""){
+                val name = eteNombre.text.toString()
 
+                val bundle= Bundle()
+                bundle.putString("NOMBRE_EJERCICIO", name)
 
+                val fragSeries= SeriesFragment()
+                fragSeries.arguments= bundle
+
+                //Ir a FragmentSeries
+                val ft = requireActivity().supportFragmentManager.beginTransaction()
+                ft.replace(R.id.fragment_container_main, fragSeries)
+                ft.addToBackStack(null)
+                ft.commit()
+            }
+            else{
+                Toast.makeText(context, "Ingrese nombre de ejercicio", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
-
 
 }
